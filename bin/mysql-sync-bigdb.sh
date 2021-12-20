59d0261d9dc043eaf260f2a1db4a9f8375e59fb3
garnier-quentin <garnier.quentin@gmail.com>
2021-12-20 12:00:30 +0100

    wip

--- bin/mysql-sync-bigdb.sh
+++ bin/mysql-sync-bigdb.sh
@@ -57,8 +57,8 @@ fi
 
 which rsync > /dev/null
 if [ "$?" -ne "0" ] ; then
-	echo "ERROR: Need rsync command." >&2
-	exit 1
+    echo "ERROR: Need rsync command." >&2
+    exit 1
 fi
 
 ###
@@ -71,75 +71,75 @@ started=0
 # Find datadir
 ###
 if [ -n "$process" ] ; then
-	datadir=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--datadir")) { print $i } } }' | awk -F\= '{ print $2 }')
-	etc_file=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--defaults-file")) { print $i } } }' | awk -F\= '{ print $2 }')
-	logbin=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--log-bin")) { print $i } } }' | awk -F\= '{ print $1 }')
-	logbin_path=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--log-bin")) { print $i } } }' | awk -F\= '{ print $2 }')
-	pidname=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--pid-file")) { print $i } } }' | awk -F\= '{ print $2 }')
-	relaylog=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--relay-log")) { print $i } } }' | awk -F\= '{ print $1 }')
-	relaylog_path=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--relay-log")) { print $i } } }' | awk -F\= '{ print $2 }')
-	started=1
+    datadir=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--datadir")) { print $i } } }' | awk -F\= '{ print $2 }')
+    etc_file=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--defaults-file")) { print $i } } }' | awk -F\= '{ print $2 }')
+    logbin=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--log-bin")) { print $i } } }' | awk -F\= '{ print $1 }')
+    logbin_path=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--log-bin")) { print $i } } }' | awk -F\= '{ print $2 }')
+    pidname=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--pid-file")) { print $i } } }' | awk -F\= '{ print $2 }')
+    relaylog=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--relay-log")) { print $i } } }' | awk -F\= '{ print $1 }')
+    relaylog_path=$(echo "$process" | awk '{ for (i = 1; i < NF; i++) { if (match($i, "--relay-log")) { print $i } } }' | awk -F\= '{ print $2 }')
+    started=1
 	if [ -n "$etc_file" ] ; then
-		MYSQL_CNF="$etc_file"
-	fi
+        MYSQL_CNF="$etc_file"
+    fi
 fi
 
 if [ -z "$datadir" ] ; then
-	datadir=$(cat $MYSQL_CNF | grep -E '^datadir' | awk -F\= '{ print $2; exit 0 }')
+    datadir=$(cat $MYSQL_CNF | grep -E '^datadir' | awk -F\= '{ print $2; exit 0 }')
 fi
 
 if [ -z "$datadir" ] ; then
-	echo "ERROR: Can't find MySQL datadir." >&2
-	exit 1
+    echo "ERROR: Can't find MySQL datadir." >&2
+    exit 1
 fi
 ### Avoid datadir is a symlink (get the absolute path)
 datadir=$(cd "$datadir"; pwd -P)
 
 if [ -z "$pidname" ] ; then
-	pidname=$(cat "$MYSQL_CNF" | grep -E '^pid-file' | awk -F\= '{ print $2; exit 0 }')
+    pidname=$(cat "$MYSQL_CNF" | grep -E '^pid-file' | awk -F\= '{ print $2; exit 0 }')
 fi
 if [ -z "$pidname" ] ; then
-	pidname=$(hostname | cut -d '.' -f 1)
+    pidname=$(hostname | cut -d '.' -f 1)
 else
-	pidname=$(basename "$pidname" | cut -d '.' -f 1)
+    pidname=$(basename "$pidname" | cut -d '.' -f 1)
 fi
 
 if [ -z "$logbin" ] ; then
-	logbin=$(cat "$MYSQL_CNF" | grep -E '^log-bin' | awk -F\= '{ print $1 }')
-	logbin_path=$(cat "$MYSQL_CNF" | grep -E '^log-bin' | awk -F\= '{ print $2 }')
+    logbin=$(cat "$MYSQL_CNF" | grep -E '^log-bin' | awk -F\= '{ print $1 }')
+    logbin_path=$(cat "$MYSQL_CNF" | grep -E '^log-bin' | awk -F\= '{ print $2 }')
 fi
 if [ -z "$logbin" ] ; then
-	echo "'log-bin' option not found. Can't sync."
-	exit 1
+    echo "'log-bin' option not found. Can't sync."
+    exit 1
 else
-	if [ -n "$logbin_path" ] ; then
-		logbin_files=$(basename "$logbin_path")
-		logbin_loc=$(dirname "$logbin_path")
-	else
-		logbin_files="$pidname-bin"
-	fi
-	if [ -z "$logbin_loc" ] || [ "$logbin_loc" = "." ] ; then
-		logbin_loc="$datadir"
-	fi
+    if [ -n "$logbin_path" ] ; then
+        logbin_files=$(basename "$logbin_path")
+        logbin_loc=$(dirname "$logbin_path")
+    else
+        logbin_files="$pidname-bin"
+    fi
+    if [ -z "$logbin_loc" ] || [ "$logbin_loc" = "." ] ; then
+        logbin_loc="$datadir"
+    fi
 fi
 
 if [ -z "$relaylog" ] ; then
-	relaylog=$(cat "$MYSQL_CNF" | grep -E '^relay-log' | awk -F\= '{ print $1 }')
-	relaylog_path=$(cat "$MYSQL_CNF" | grep -E '^relay-log' | awk -F\= '{ print $2 }')
+    relaylog=$(cat "$MYSQL_CNF" | grep -E '^relay-log' | awk -F\= '{ print $1 }')
+    relaylog_path=$(cat "$MYSQL_CNF" | grep -E '^relay-log' | awk -F\= '{ print $2 }')
 fi
 if [ -z "$relaylog" ] ; then
-	relaylog_loc="$datadir"
-	relaylog_files="$pidname-relay-bin"
+    relaylog_loc="$datadir"
+    relaylog_files="$pidname-relay-bin"
 else
-	if [ -n "$relaylog_path" ] ; then
-		relaylog_files=$(basename "$relaylog_path")
-		relaylog_loc=$(dirname "$relaylog_path")
-	else
-		relaylog_files="$pidname-relay-bin"
-	fi
-	if [ -z "$relaylog_loc" ] ; then
-		relaylog_loc="$datadir"
-	fi
+    if [ -n "$relaylog_path" ] ; then
+        relaylog_files=$(basename "$relaylog_path")
+        relaylog_loc=$(dirname "$relaylog_path")
+    else
+        relaylog_files="$pidname-relay-bin" 
+    fi
+    if [ -z "$relaylog_loc" ] ; then
+        relaylog_loc="$datadir"
+    fi
 fi
 
 echo "MySQL datadir found: $datadir"
@@ -158,31 +158,15 @@ echo "MySQL relaylog localisation: $relaylog_loc"
 mount_device=$(df -P "$datadir" | tail -1 | awk '{ print $1 }')
 mount_point=$(df -P "$datadir" | tail -1 | awk '{ print $6 }')
 if [ -z "$mount_device" ] ; then
-	echo "ERROR: Can't get mount device for datadir." >&2
-	exit 1
+    echo "ERROR: Can't get mount device for datadir." >&2
+    exit 1
 fi
 if [ -z "$mount_point" ] ; then
-	echo "ERROR: Can't get mount point for datadir." >&2
-	exit 1
+    echo "ERROR: Can't get mount point for datadir." >&2
+    exit 1
 fi
 echo "Mount device 'datadir' found: $mount_device"
 echo "Mount point 'datadir' found: $mount_point"
-
-###
-# Get mount logbin
-###
-mount_device_logbin=$(df -P "$logbin_loc" | tail -1 | awk '{ print $1 }')
-mount_point_logbin=$(df -P "$logbin_loc" | tail -1 | awk '{ print $6 }')
-if [ -z "$mount_device_logbin" ] ; then
-	echo "ERROR: Can't get mount device for log-bin dir." >&2
-	exit 1
-fi
-if [ -z "$mount_point_logbin" ] ; then
-	echo "ERROR: Can't get mount point for log-bin dir." >&2
-	exit 1
-fi
-echo "Mount device 'log-bin' found: $mount_device_logbin"
-echo "Mount point 'log-bin' found: $mount_point_logbin"
 
 ###
 # Get Volume group Name
@@ -190,33 +174,16 @@ echo "Mount point 'log-bin' found: $mount_point_logbin"
 vg_name=$(lvdisplay -c "$mount_device" | cut -d : -f 2)
 lv_name=$(lvdisplay -c "$mount_device" | cut -d : -f 1)
 if [ -z "$vg_name" ] ; then
-	echo "ERROR: Can't get VolumeGroup name for datadir." >&2
-	exit 1
+    echo "ERROR: Can't get VolumeGroup name for datadir." >&2
+    exit 1
 fi
 if [ -z "$lv_name" ] ; then
-	echo "ERROR: Can't get LogicalVolume name for datadir." >&2
-	exit 1
-fi
-
-vg_name_logbin=$(lvdisplay -c "$mount_device_logbin" | cut -d : -f 2)
-lv_name_logbin=$(lvdisplay -c "$mount_device_logbin" | cut -d : -f 1)
-if [ -z "$vg_name_logbin" ] ; then
-	echo "ERROR: Can't get VolumeGroup name for log-bin dir." >&2
-	exit 1
-fi
-if [ -z "$lv_name_logbin" ] ; then
-	echo "ERROR: Can't get LogicalVolume name for log-bin dir." >&2
-	exit 1
-fi
-
-if [ "$vg_name_logbin" != "$vg_name" ] ; then
-	echo "ERROR: log-bin dir and datadir have to be on the same VolumeGroup." >&2
-	exit 1
+    echo "ERROR: Can't get LogicalVolume name for datadir." >&2
+    exit 1
 fi
 
 echo "VolumeGroup found: $vg_name"
 echo "LogicalVolume 'datadir' found: $lv_name"
-echo "LogicalVolume 'log-bin' found: $lv_name_logbin"
 
 ###
 # Get free Space
@@ -225,12 +192,12 @@ echo "LogicalVolume 'log-bin' found: $lv_name_logbin"
 free_pe=$(vgdisplay -c "$vg_name" | cut -d : -f 16)
 size_pe=$(vgdisplay -c "$vg_name" | cut -d : -f 13)
 if [ -z "$free_pe" ] ; then
-	echo "ERROR: Can't get free PE value for the VolumeGroup." >&2
-	exit 1
+    echo "ERROR: Can't get free PE value for the VolumeGroup." >&2
+    exit 1
 fi
 if [ -z "$size_pe" ] ; then
-	echo "ERROR: Can't get size PE value for the VolumeGroup." >&2
-	exit 1
+    echo "ERROR: Can't get size PE value for the VolumeGroup." >&2
+    exit 1
 fi
 
 free_total_pe=$(echo $free_pe " " $size_pe | awk '{ print ($1 * $2) / 1024 / 1024 }')
@@ -238,8 +205,8 @@ echo "Free total size in VolumeGroup (Go): $free_total_pe"
 
 echo "$free_total_pe $VG_FREESIZE_NEEDED" | awk '{ if ($2 > $1) { exit(1) } else { exit(0) } }'
 if [ "$?" -eq 1 ] ; then
-	echo "ERROR: Not enough free space in the VolumeGroup." >&2
-	exit 1
+    echo "ERROR: Not enough free space in the VolumeGroup." >&2
+    exit 1
 fi
 
 ###
@@ -251,8 +218,8 @@ master_hostname=$(get_other_db_hostname $slave_hostname)
 echo "Connection to slave Server (verify mysql stopped): $slave_hostname"
 result=$($USER_SUDO ssh -p $SSH_PORT $slave_hostname 'if ps --no-headers -C '"$MYSQLBINARY"' >/dev/null; then echo "yes" ; else echo "no"; fi')
 if [ "$result" != "no" ] ; then
-	echo "ERROR: MySQL is launched or problem to connect to the server." >&2
-	exit 1
+    echo "ERROR: MySQL is launched or problem to connect to the server." >&2
+    exit 1
 fi
 if [ "$started" -eq 0 ] ; then
     echo "ERROR: MySQL master is not started." >&2
@@ -292,10 +259,10 @@ echo "OK"
 ###
 echo "Create LVM snapshot"
 if [ "$lv_name_logbin" != "$lv_name" ] ; then
-	lvcreate -l $(($free_pe / 2)) -s -n dbbackupdatadir $lv_name
-	lvcreate -l $(($free_pe / 2)) -s -n dbbackuplogbin $lv_name_logbin
+    lvcreate -l $(($free_pe / 2)) -s -n dbbackupdatadir $lv_name
+    lvcreate -l $(($free_pe / 2)) -s -n dbbackuplogbin $lv_name_logbin
 else
-	lvcreate -l $free_pe -s -n dbbackupdatadir $lv_name
+    lvcreate -l $free_pe -s -n dbbackupdatadir $lv_name
 fi
 
 ###
@@ -305,14 +272,14 @@ echo "Start $MYSQLBINARY: ($MYSQL_START)"
 $MYSQL_START &
 i=0
 until mysqlshow -u "$DBROOTUSER" -h "$master_hostname" -p"$DBROOTPASSWORD" > /dev/null 2>&1; do
-	if [ "$i" -gt "$STOP_TIMEOUT" ] ; then
-		echo ""
-		echo "ERROR: Can't start MySQL server" >&2
-		exit 1
-	fi
-	echo -n "."
-	sleep 1
-	i=$(($i + 1))
+    if [ "$i" -gt "$STOP_TIMEOUT" ] ; then
+        echo ""
+        echo "ERROR: Can't start MySQL server" >&2
+        exit 1
+    fi
+    echo -n "."
+    sleep 1
+    i=$(($i + 1))
 done
 echo "OK"
 
